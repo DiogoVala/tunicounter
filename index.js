85f007fd27bcbdc43a444c8765beea7c15ed72d5
Joao Pandeirada <joao.pandeirada@ua.pt>
2022-10-04 23:50:20 +0100

    keyboard listener for tapping and flipping added

--- index.js
+++ index.js
@@ -18,50 +18,41 @@ var keys;
 function preload ()
 {
     this.load.image('bg', 'assets/FABPlayMat.webp');
-    this.load.image('card', 'assets/ARC121.jpg');
-    this.load.image('card1', 'assets/ARC000.jpg');
+    this.load.spritesheet("arc000", "assets/ARC000.png",  { frameWidth: 452, frameHeight: 648 });
+    this.load.spritesheet("arc121", "assets/ARC121.png",  { frameWidth: 452, frameHeight: 648 });
 }
 
+var keys;
+var card, card1;
+var pointerover = false;
 
 function create ()
 {
     var bg = this.add.image(1280/2-100, 720/2, 'bg');
     bg.setScale(1.5)
 
-    var card = new Card(this)
-    card.render(718/2, 420/2, 'card')
+    
+    card = new Card(this, 718/2, 420/2, 'arc000')
+    card1 = new Card(this, 718/2 + 100, 420/2, 'arc121')
 
-    var card1 = new Card(this)
-    card1.render(718/2+100, 420/2, 'card1')
-    /*var card = this.add.image(718/2, 420/2, 'card').setInteractive();
-    card_new = this.add.image(1300, 350, 'card');
-    card_new.setAlpha(0)
-    keys = this.input.keyboard.addKeys('F');
-
-    card.setScale(0.2)
-    bg.setScale(1.5)
-    this.input.setDraggable(card);
-
-    card.on('pointerover', function () {
-        card_new.setAlpha(1)
-    });
-
-    card.on('pointerout', function () {
-        card_new.setAlpha(0)
-    });*/
-
+    keys = this.input.keyboard.addKeys('T,F');
+   
     this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
 
         gameObject.x = dragX;
         gameObject.y = dragY;
 
     });
-
-
-    
+            
 }
 
 function update ()
-{
+{   
+    if(Phaser.Input.Keyboard.JustDown(keys.T) && card.pointerover){
+        card.tap()
+    }
 
+    if(Phaser.Input.Keyboard.JustDown(keys.F) && card.pointerover){
+        card.flip()
+    }
 }
