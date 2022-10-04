f429f6efd32f8aaf0c9602b6cf8d0adc3ee9f3c7
Joao Pandeirada <joao.pandeirada@ua.pt>
2022-10-04 23:49:43 +0100

    card class now extends spritesheet and has tap and flip functionality

--- card.js
+++ card.js
@@ -1,21 +1,49 @@
-export default class Card {
-    constructor(scene) {
-        this.render = (x, y, sprite) => {
-            let card = scene.add.image(x, y, sprite).setInteractive();
-            let card_augmented = scene.add.image(1300, 350, sprite);
-            card.setScale(0.2)
-            card_augmented.setAlpha(0)
-            scene.input.setDraggable(card);
-            
-            card.on('pointerover', function () {
-                card_augmented.setAlpha(1)
-            });
+export default class Card extends Phaser.GameObjects.Sprite{
+    pointerover = false;
+    tapped = false;
+    constructor(scene, x, y, sprite){
+        super(scene, x, y, sprite);
+
+        scene.add.existing(this)
+        this.setInteractive();
+        this.card_augmented = scene.add.sprite(1300, 350, sprite);
         
-            card.on('pointerout', function () {
-                card_augmented.setAlpha(0)
-            });
-            
-            return card;
+        this.card_augmented.setAlpha(0)
+        this.setScale(0.2);
+        scene.input.setDraggable(this);
+
+        this.on('pointerover', function () {
+
+            this.card_augmented.setAlpha(1);
+            this.pointerover = true;
+        });
+        
+        this.on('pointerout', function () {
+            this.card_augmented.setAlpha(0);
+            this.pointerover = false;
+        });
+    }
+
+    tap(){
+        if (!this.tapped){
+            this.rotation += 3.14/2;
+            this.tapped = true;
+        }
+        else{
+            this.rotation -= 3.14/2;
+            this.tapped = false;
+        }
+        
+    }
+
+    flip(){
+        if(this.frame.name == 0){
+            this.setFrame(1);
+            this.card_augmented.setFrame(1);
+        }
+        else{
+            this.setFrame(0);
+            this.card_augmented.setFrame(0);
         }
     }
 }
