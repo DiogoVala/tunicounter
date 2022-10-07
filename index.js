3c0fb9fbc63b74f9c6334bb3708f577ac87ab7ad
Joao Pandeirada <joao.pandeirada@ua.pt>
2022-10-07 22:12:41 +0100

    added dice and update routine to roll and set dice, needs improvement

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Card from './card.js';
+import Dice from './dice.js';
 
 var config = {
     type: Phaser.AUTO,
@@ -20,9 +21,10 @@ function preload ()
     this.load.image('bg', 'assets/FABPlayMat.webp');
     this.load.spritesheet("arc000", "assets/ARC000.png",  { frameWidth: 452, frameHeight: 648 });
     this.load.spritesheet("arc121", "assets/ARC121.png",  { frameWidth: 452, frameHeight: 648 });
+    this.load.spritesheet("dice", "assets/dice_sheet.png",  { frameWidth: 128, frameHeight: 128 });
 }
 
-var keys;
+var keys, dice, numbers;
 var cards_on_board = [];
 
 function create ()
@@ -34,8 +36,11 @@ function create ()
     cards_on_board.push(new Card(this, 718/2, 420/2, 'arc000'));
     cards_on_board.push(new Card(this, 718/2 + 100, 420/2, 'arc121'));
 
-    keys = this.input.keyboard.addKeys('T,F');
-   
+    dice = new Dice(this,200,200, 'dice')
+
+    keys = this.input.keyboard.addKeys('T,F,R');
+    numbers = this.input.keyboard.addKeys('ONE,TWO,THREE,FOUR,FIVE,SIX');
+
     this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
 
         gameObject.x = dragX;
@@ -61,11 +66,37 @@ function update ()
             card.flip();
         }
     }
+
+    if(dice.pointerover){
+        if(Phaser.Input.Keyboard.JustDown(keys.R)){
+            dice.roll();
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(numbers.ONE)){
+            dice.setSide(1);
+        }
+        else if(Phaser.Input.Keyboard.JustDown(numbers.TWO)){
+            dice.setSide(2);
+        }
+        else if(Phaser.Input.Keyboard.JustDown(numbers.THREE)){
+            dice.setSide(3);
+        }
+        else if(Phaser.Input.Keyboard.JustDown(numbers.FOUR)){
+            dice.setSide(4);
+        }
+        else if(Phaser.Input.Keyboard.JustDown(numbers.FIVE)){
+            dice.setSide(5);
+        }
+        else if(Phaser.Input.Keyboard.JustDown(numbers.SIX)){
+            dice.setSide(6);
+        }
+    }
+
 }
 
 function overedCard(cards){
     var return_card = false;
-    
+    var card;
     for (card of cards){
         if (card.pointerover){
             return_card = card;
