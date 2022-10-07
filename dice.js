be2a2afe4ff88e9be2a0cd31f80646e759e0e7e9
Joao Pandeirada <joao.pandeirada@ua.pt>
2022-10-07 22:15:33 +0100

    created dice class and added dice spritesheet to assets

---
+++ dice.js
@@ -0,0 +1,49 @@
+export default class Dice extends Phaser.GameObjects.Sprite{
+    pointerover = false;
+    tapped = false;
+    constructor(scene, x, y, sprite){
+        super(scene, x, y, sprite);
+
+        scene.add.existing(this)
+        this.setInteractive();
+
+        this.setScale(0.5);
+        scene.input.setDraggable(this);
+
+        scene.anims.create({
+            key: 'roll',
+            frames: scene.anims.generateFrameNames('dice'),
+            frameRate: 15,
+            yoyo: true,
+            repeat: 1,
+            repeatDelay: 0
+        });
+
+        this.on('pointerover', function () {
+            this.pointerover = true;
+        });
+        
+        this.on('pointerout', function () {
+            this.pointerover = false;
+        });
+
+        this.on('dragstart', function () {
+            scene.children.bringToTop(this);
+        });
+
+        this.on('animationcomplete', function () {
+            this.setSide(Math.floor(Math.random()*6)+1);
+            this.setAlpha(1);
+        });
+    }
+
+    setSide(num){
+        this.setFrame(num-1);
+    }
+
+    roll(){
+        this.setAlpha(0.5);
+        this.play('roll');
+    }
+
+}
